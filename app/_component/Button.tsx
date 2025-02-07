d79f667ff9125b2b3d7701b1f846392a2f84f1ac
lee juyoung <juyung0903@naver.com>
2025-02-07 15:57:52 +0900

    feat: Button 컴포넌트

---
+++ app/_component/Button.tsx
@@ -0,0 +1,61 @@
+import { ReactNode } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+function FlatButton({
+  children,
+  onPress,
+  theme = "default",
+}: {
+  children: ReactNode;
+  onPress: () => void;
+  theme?: string;
+}) {
+  return (
+    <Pressable
+      style={({ pressed }) => [
+        { backgroundColor: `${theme === "default" ? "#6500A8" : "white"}` },
+        styles.button,
+        pressed && styles.pressed,
+      ]}
+      onPress={onPress}
+    >
+      <View>
+        <Text
+          style={[
+            styles.text,
+            { color: `${theme === "default" ? "#ffffff" : "#6500A8"}` },
+          ]}
+        >
+          {children}
+        </Text>
+      </View>
+    </Pressable>
+  );
+}
+
+export default FlatButton;
+
+const styles = StyleSheet.create({
+  button: {
+    paddingVertical: 14,
+    paddingHorizontal: 134,
+    borderRadius: 6,
+    elevation: 2,
+    shadowColor: "black",
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+  text: {
+    fontFamily: "Roboto",
+    fontSize: 18,
+    fontWeight: "500",
+    lineHeight: 28,
+    textAlign: "center",
+    letterSpacing: 0,
+    color: "#ffffff",
+  },
+});
