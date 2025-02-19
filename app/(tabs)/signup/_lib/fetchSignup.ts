c22e7ba25c3e91b5a55dae708bea940339dbd7d4
lee juyoung <juyung0903@naver.com>
2025-02-19 11:33:39 +0900

    fix: 회원가입 api요청부분 잘못된 url 수정

--- app/(tabs)/signup/_lib/fetchSignup.ts
+++ app/(tabs)/signup/_lib/fetchSignup.ts
@@ -4,7 +4,7 @@ import { userSignupType } from "@/app/_types";
 
 export async function fetchSignup(data: userSignupType) {
   try {
-    const response = await axios.post(`${process.env.API_URL}/user/register`, data);
+    const response = await axios.post(`${process.env.EXPO_PUBLIC_API_URL}/user/register`, data);
     return response;
   } catch (error) {
     Alert.alert("회원가입에 실패했습니다.");
