816c76b170d6653a464a8445587d9665bbe11c14
lee juyoung <juyung0903@naver.com>
2025-04-05 19:08:53 +0900

    chore:  sentry를 통한 장소검색 훅 검사

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,16 @@ import TabBar from "./_component/TabBar";
 import Header from "./_component/Header";
 import { LocationProvider } from "./_hooks/useLocation";
 import AuthContextProvider from "./_hooks/useAuth";
+import * as Sentry from '@sentry/react-native';
 
-export default function TabLayout() {
+Sentry.init({
+  dsn: 'https://80009cc741bd92843277d20ecf1a31dc@o4509099095293952.ingest.us.sentry.io/4509099096145920',
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function TabLayout() {
   return (
     <AuthContextProvider>
       <LocationProvider>
@@ -37,4 +45,4 @@ export default function TabLayout() {
       </LocationProvider>
     </AuthContextProvider>
   );
-}
+});
