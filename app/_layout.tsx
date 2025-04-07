7e1d2a2d07832c41cf6e2fcb4fc0cb40f6a241d0
lee juyoung <juyung0903@naver.com>
2025-04-07 23:44:07 +0900

    setting:tanstack/query 세팅

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,38 +3,59 @@ import TabBar from "./_component/TabBar";
 import Header from "./_component/Header";
 import { LocationProvider } from "./_hooks/useLocation";
 import AuthContextProvider from "./_hooks/useAuth";
+import { DevToolsBubble } from "react-native-react-query-devtools";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import * as Clipboard from "expo-clipboard";
+
+const queryClient = new QueryClient();
 
 export default function TabLayout() {
+
+  const onCopy = async (text: string) => {
+    try {
+      // For Expo:
+      await Clipboard.setStringAsync(text);
+      // OR for React Native CLI:
+      // await Clipboard.setString(text);
+      return true;
+    } catch {
+      return false;
+    }
+  };
   return (
-    <AuthContextProvider>
-      <LocationProvider>
-      <Header />
-        <Tabs
-        screenOptions={{headerShown:false}}
-          tabBar={() => <TabBar />}
-        >
-
-          <Tabs.Screen
-            name="index"
-            options={{
-              title: "Intro",
-              headerShown: false, // 헤더를 숨김
-              tabBarStyle: { display: "none" }, // 탭바 숨김
-            }}
-          />
-          <Tabs.Screen
-            name="login"
-            options={{ title: "login", headerShown: false }}
-          />
-          <Tabs.Screen
-            name="signup"
-            options={{ title: "signup", headerShown: false }}
-          />
-          <Tabs.Screen name="map" options={{ title: "map" }} />
-          <Tabs.Screen name="crew" options={{ title: "crew" }} />
-
-        </Tabs>
-      </LocationProvider>
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <LocationProvider>
+          <Header />
+          <Tabs
+            screenOptions={{ headerShown: false }}
+            tabBar={() => <TabBar />}
+          >
+            <Tabs.Screen
+              name="index"
+              options={{
+                title: "Intro",
+                headerShown: false, // 헤더를 숨김
+                tabBarStyle: { display: "none" }, // 탭바 숨김
+              }}
+            />
+            <Tabs.Screen
+              name="login"
+              options={{ title: "login", headerShown: false }}
+            />
+            <Tabs.Screen
+              name="signup"
+              options={{ title: "signup", headerShown: false }}
+            />
+            <Tabs.Screen name="map" options={{ title: "map" }} />
+            <Tabs.Screen name="crew" options={{ title: "crew" }} />
+          </Tabs>
+        </LocationProvider>
+      </AuthContextProvider>
+      <DevToolsBubble onCopy={onCopy} />
+    </QueryClientProvider>
   );
 }
