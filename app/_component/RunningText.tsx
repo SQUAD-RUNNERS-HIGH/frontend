d31337f50f5f9af9afe1f7fa9f30cb56990a1565
lee juyoung <juyung0903@naver.com>
2025-04-25 20:23:56 +0900

    feat: 코스에서 벗어났을시 음성 취소

--- app/_component/RunningText.tsx
+++ app/_component/RunningText.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import * as Speech from "expo-speech";
+import { useLocation } from "../_hooks/useLocation";
 
 export const RunningText = ({
   winning,
@@ -12,6 +13,7 @@ export const RunningText = ({
   seconds: number;
 }) => {
   const [message, setMessage] = useState<string>();
+  const {runningLocation} = useLocation();
   useEffect(() => {
     setMessage(
       `${distance.toFixed(0)}m ${winning ? "앞서고 있어요" : "뒤처지고 있어요"}`
@@ -19,7 +21,11 @@ export const RunningText = ({
   }, [distance]);
   useEffect(() => {
     if (seconds % 3 === 0 && message) {
+      Speech.stop(); // 이전 스피치 중단
       Speech.speak(message, { voice: "ko-KR-SMTl01" });
+    }
+    if (runningLocation?.runningStatus === 'ESCAPED') {
+      Speech.stop();
     }
   }, [seconds]);
   return (
