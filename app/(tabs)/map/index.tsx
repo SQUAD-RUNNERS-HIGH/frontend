32722b52af1f848aaa13bca7bd4951594bf00398
lee juyoung <juyung0903@naver.com>
2025-04-29 15:14:34 +0900

    feat: 러닝할때 시점 변경

--- app/(tabs)/map/index.tsx
+++ app/(tabs)/map/index.tsx
@@ -17,6 +17,7 @@ import { fetchCourses } from "./_lib/fetchCourses";
 import RunningInfo from "@/app/_component/Modal/RunningModal/RunningInfo";
 import { useQuery } from "@tanstack/react-query";
 import { fetchMyCrew } from "../crew/_lib/fetchMyCrew";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 export default function Index() {
   useQuery({
     queryKey: ["myCrew"],
@@ -24,7 +25,6 @@ export default function Index() {
     staleTime: 100000,
   });
   const [region, setRegion] = useState<Region>();
-
   const { data, isLoading, error } = useQuery({
     queryKey: ['courses', region],
     queryFn: () => fetchCourses(region),
@@ -88,31 +88,48 @@ export default function Index() {
   // 러닝 시
   useEffect(() => {
     // 지도 중심을 새로운 위치로 이동
+    const PITCH = 0.0005
     if (isRunning && runningLocation && myLocation) {
       mapRef.current?.animateCamera({
         center: {
-          latitude: runningLocation?.latitude,
+          latitude: myLocation?.latitude-PITCH,
           longitude: runningLocation?.longitude,
         },
-        pitch: 0, // 기울기 (0~90도)
+        pitch: 50, // 기울기 (0~90도)
         heading: runningLocation?.heading, // 방향 (나아가는 방향)
         altitude: runningLocation?.altitude, // 고도
         zoom: 18, // 줌 레벨
       });
     }
-    if (isRunning && !runningLocation && myLocation) {
+    if (isRunning && runningInfo === 'solo' && myLocation) {
       mapRef.current?.animateCamera({
         center: {
-          latitude: myLocation?.latitude,
+          latitude: myLocation?.latitude-PITCH,
           longitude: myLocation?.longitude,
         },
-        pitch: 0, // 기울기 (0~90도)
+        pitch: 50, // 기울기 (0~90도)
         heading: myLocation?.heading, // 방향 (나아가는 방향)
         altitude: myLocation?.altitude, // 고도
-        zoom: 18, // 줌 레벨
+        zoom: 17, // 줌 레벨
       });
     }
-  }, [isRunning, myLocation, runningLocation]);
+    if(!isRunning && runningInfo.includes('Finish')){
+
+        mapRef.current?.animateCamera(
+          {
+            center: {
+              longitude: myLocation?.longitude,
+              latitude: myLocation?.latitude,
+            },
+            zoom:16,
+            pitch: 0,
+            altitude:myLocation?.altitude,
+          },
+          { duration: 1000 }
+        );
+      }
+    } 
+  , [isRunning, myLocation, runningLocation]);
 
   // 위치 추적 시작
   useEffect(() => {
@@ -248,14 +265,15 @@ export default function Index() {
             onPress={async () => {
               const currentCamera = await mapRef.current?.getCamera();
               if (currentCamera) {
-                const { center, zoom, ...rest } = currentCamera;
+                const { center, zoom,pitch, ...rest } = currentCamera;
                 mapRef.current?.animateCamera(
                   {
                     center: {
                       longitude: myLocation?.longitude,
                       latitude: myLocation?.latitude,
                     },
-                    zoom,
+                    zoom:16,
+                    pitch: 0,
                     ...rest,
                   },
                   { duration: 1000 }
@@ -281,14 +299,15 @@ export default function Index() {
             onPress={async () => {
               const currentCamera = await mapRef.current?.getCamera();
               if (currentCamera) {
-                const { center, zoom, ...rest } = currentCamera;
+                const { center, zoom,pitch, ...rest } = currentCamera;
                 mapRef.current?.animateCamera(
                   {
                     center: {
                       longitude: myLocation?.longitude,
                       latitude: myLocation?.latitude,
                     },
-                    zoom,
+                    zoom:16,
+                    pitch: 0,
                     ...rest,
                   },
                   { duration: 1000 }
