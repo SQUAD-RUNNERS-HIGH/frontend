f9bda4d79a59242d22ad5fbeba5e60b9afe5a4e3
lee juyoung <juyung0903@naver.com>
2025-04-30 11:16:47 +0900

    feat: 이미지 업로드 contain 추가

--- app/_component/FormInput/ImageUpload.tsx
+++ app/_component/FormInput/ImageUpload.tsx
@@ -36,7 +36,9 @@ const ImageUpload = ({
       {field.value ? (
         <ImageBackground
           source={{ uri: field.value }}
-          style={{ width: '100%', height: '100%' }}
+          style={{ width: '100%', height:'100%' }}
+          resizeMode="contain"
+
         />
       ) : (
         <>
