af48d9a5e2c2e14ab22216178aa2b3c0283bc35f
lee juyoung <juyung0903@naver.com>
2025-04-30 10:54:21 +0900

    fix: 크루만들기 발표때문에 image없이 롤백

--- app/(tabs)/crew/_lib/fetchCrewCreate.ts
+++ app/(tabs)/crew/_lib/fetchCrewCreate.ts
@@ -1,46 +1,18 @@
-import { apiClient } from "@/api/apiClient";
+import axios from "axios";
+import { Alert } from "react-native";
+import { CreateCrewApi, userSignupType } from "@/app/_types";
 import { z } from "zod";
 import { crewSchema } from "./crewSchema";
-import { Alert } from "react-native";
+import { apiClient } from "@/api/apiClient";
 
 export async function fetchCrewCreate(data: z.infer<typeof crewSchema>) {
   try {
-    const formData = new FormData();
-
-    // crewCreateRequest 객체를 만들기 위해 필요한 필드 모음
-    const crewCreateRequest: any = {};
-
-    Object.entries(data).forEach(([key, value]) => {
-      if (key === "crewLocation" && typeof value === "object" && value !== null) {
-        crewCreateRequest[key] = value;
-      } else if (key === "image" && typeof value === "string") {
-        const uri = value;
-        const filename = uri.split('/').pop() ?? 'photo.jpg';
-
-        formData.append('image', {
-          uri,
-          name: filename,
-          type: 'image/jpeg', // 무조건 image/jpeg로 고정
-        } as any);
-        
-      } else {
-        crewCreateRequest[key] = value;
-      }
-    });
-    console.log(formData);
-    // crewCreateRequest 부분 추가
-    formData.append('crewCreateRequest', JSON.stringify(crewCreateRequest));
-
-    const response = await apiClient.post(`/crew`, formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    });
-
+    const {image, ...rest} = data;
+    const response = await apiClient.post(`/crew`, rest);
     return response;
-  } catch (error: any) {
+  } catch (error) {
     if (error?.response) {
-      Alert.alert(error.response.data.serverErrorMessage || "에러 발생");
+      Alert.alert(error.response.data.serverErrorMessage);
     }
   }
 }
