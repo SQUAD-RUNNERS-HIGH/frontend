fcdca404ea8a397390c98ec763242694ed13ef0a
lee juyoung <juyung0903@naver.com>
2025-06-17 12:18:15 +0900

    feat: SafeAreaView

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,31 +5,36 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import * as Clipboard from "expo-clipboard";
 import { useReactQueryDevTools } from "@dev-plugins/react-query";
 import GlobalModal from "@/component/GlobalModal";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 const queryClient = new QueryClient();
 export default function TabLayout() {
   useReactQueryDevTools(queryClient);
   return (
     <QueryClientProvider client={queryClient}>
-      <Header />
-      <GlobalModal />
+      <StatusBar style="light" backgroundColor="black" translucent={false} />
+      <SafeAreaView style={{ flex: 1 }}>
+        <Header />
+        <GlobalModal />
 
-      <Tabs screenOptions={{ headerShown: false }} tabBar={() => <TabBar />}>
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: "index",
-            tabBarStyle: { display: "none" }, // 탭바 숨김
-          }}
-        />
-        <Tabs.Screen name="login" options={{ title: "login" }} />
-        <Tabs.Screen
-          name="signup"
-          options={{ title: "signup", headerShown: false }}
-        />
-        <Tabs.Screen name="map" options={{ title: "map" }} />
-        <Tabs.Screen name="crew" options={{ title: "crew" }} />
-      </Tabs>
+        <Tabs screenOptions={{ headerShown: false }} tabBar={() => <TabBar />}>
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: "index",
+              tabBarStyle: { display: "none" }, // 탭바 숨김
+            }}
+          />
+          <Tabs.Screen name="login" options={{ title: "login" }} />
+          <Tabs.Screen
+            name="signup"
+            options={{ title: "signup", headerShown: false }}
+          />
+          <Tabs.Screen name="map" options={{ title: "map" }} />
+          <Tabs.Screen name="crew" options={{ title: "crew" }} />
+        </Tabs>
+      </SafeAreaView>
     </QueryClientProvider>
   );
 }
